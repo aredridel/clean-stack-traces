70087f40ed5464ef2108efdefd6dd7b2463e842d
Aria Stewart <aredridel@dinhe.net>
2015-09-10 12:50:03 -0400

    Remove trailing slash too

--- clean-stack-traces.js
+++ clean-stack-traces.js
@@ -3,7 +3,7 @@
 var split = require('split');
 var through2 = require('through2');
 
-var dir = process.cwd();
+var dir = process.cwd() + '/';
 process.stdin.pipe(split(/(\n)/)).pipe(through2.obj(function (s, _, cb) {
     if (/^  at /.test(s)) {
         this.push(s.replace(dir, ''));
