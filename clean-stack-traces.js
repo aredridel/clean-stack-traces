84d11f939af727f1e92ca130d9d7748d1fc1f4d2
Aria Stewart <aredridel@dinhe.net>
2015-09-10 12:46:39 -0400

    Make it so

---
+++ clean-stack-traces.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var split = require('split');
+var through2 = require('through2');
+
+var dir = process.cwd();
+process.stdin.pipe(split(/(\n)/)).pipe(through2.obj(function (s, _, cb) {
+    if (/^  at /.test(s)) {
+        this.push(s.replace(dir, ''));
+    } else {
+        this.push(s);
+    }
+    cb();
+})).pipe(process.stdout);
